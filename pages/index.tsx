dd3820fb6496bc87c4a487a9d5504ed103d54167
bruski <292803281@qq.com>
2022-04-07 17:16:12 +0800

    feat(home): implement typist gradient text

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,17 @@ import Head from 'next/head';
 import React, { useState } from 'react';
 import Layout from '../components/Layout';
 import Image from 'next/image';
-import CarouselCapsule from '../components/CarouselCapsule';
 import { WEBSITE_TITLE } from '../constants';
+import avatarPic from '../public/avatar.png';
+import Typist from 'react-text-typist';
 
 const Home: NextPage = () => {
   const [personalTags] = useState<string[]>([
-    '👨‍💻全栈工程师',
-    '👨‍💼独立工作者',
-    '👾WEB3',
-    '🛹滑板仔',
-    '🕺B-Boy',
+    '全栈工程师',
+    '独立工作者',
+    'WEB3 BUIDLER',
+    '滑板仔',
+    'B-Boy',
   ]);
   return (
     <Layout>
@@ -26,30 +27,29 @@ const Home: NextPage = () => {
           'relative mt-6 flex w-screen flex-1 flex-col  bg-cover bg-right px-8'
         }
       >
-        <section className={'absolute right-0 bottom-0 -z-10 w-2/3'}>
-          <img
-            src={
-              'https://gitee.com/bruceeewong/image-bed/raw/master/2022-3-24/1648129483996-bruski_wang.png'
-            }
-            className={'max-w-full'}
+        <section className={'flex justify-between'}>
+          <div>
+            <h1 className={'text-3xl'}>你好，</h1>
+            <h1 className={'mt-1 text-3xl '}>我叫王思哲</h1>
+            <h4 className={'text-md mt-1'}>95后反内卷独立青年</h4>
+          </div>
+          <div className={'h-28 w-28'}>
+            <Image src={avatarPic} alt={'avatar'} className={'rounded-full'} />
+          </div>
+        </section>
+        <section className={'mt-4 flex text-xl'}>
+          <span className={'mr-1'}>我是一位</span>
+          <Typist
+            sentences={personalTags}
+            loop={true}
+            showCursor={false}
+            typingSpeed={100}
+            style={{
+              background: '-webkit-linear-gradient(0deg, #0ea5e9, #8b5cf6)',
+              '-webkit-background-clip': 'text',
+              '-webkit-text-fill-color': 'transparent',
+            }}
           />
-        </section>
-        <section className={' font-bold opacity-80'}>
-          <h1 className={'font-b old text-4xl'}>你好，</h1>
-          <h1 className={'mt-1 text-4xl '}>我叫王思哲</h1>
-          <h4 className={'mt-1 text-lg'}>95后反内卷独立青年</h4>
-        </section>
-        <section className={'mt-5'}>
-          <CarouselCapsule className={'opacity-90'} texts={personalTags} />
-          {/*<div*/}
-          {/*  className={classnames(*/}
-          {/*    'border-radius-2   rounded-full border-2 text-slate-500',*/}
-          {/*    'mt-4 self-start px-4 py-1'*/}
-          {/*  )}*/}
-          {/*>*/}
-          {/*  <span>🤣</span>*/}
-          {/*  <span className={'ml-2'}>WEB3 ONBOARDING</span>*/}
-          {/*</div>*/}
         </section>
       </div>
     </Layout>
