396a03a6d0a6586a0b8fd525743380fab664d10b
brrruski <sizhe4real@gmail.com>
2023-02-21 00:08:39 -0500

    chore: wtf is wrong with vercel build?

--- templates/project.tsx
+++ templates/project.tsx
@@ -9,8 +9,7 @@ import { projectIndex } from '../constants/project-index';
 import Image from 'next/image';
 import styles from './project.module.scss';
 import { Stack, Chip } from '@mui/material';
-import IconHome from '@mui/icons-material/Home';
-import IconGithub from '@mui/icons-material/Github';
+import { Home as IconHome, GitHub as IconGithub } from '@mui/icons-material';
 export type ProjectPageProps = {
   meta: Record<string, any>;
 };
